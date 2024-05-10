5beccb85cae1e9bacba03afe9eece1b36649eac1
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-05-10 10:13:24 +0530

    Deleted all the JavaScript files

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-
-export const YOUTUBE_API_URL =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  process.env.REACT_APP_API_KEY;
-export const YOUTUBE_SEARCH_API =
-  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-
-  export const LIVE_CHAT_COUNT= 25;
