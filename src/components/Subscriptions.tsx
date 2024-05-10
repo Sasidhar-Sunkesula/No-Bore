869a14be8e50a8cc199240f6456cc54863a3fe90
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-05-10 10:09:46 +0530

    Migrating from javascript to typescript

---
+++ src/components/Subscriptions.tsx
@@ -0,0 +1,78 @@
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromSubscriptionsList } from "../utils/subscriptionsSlice";
+import EmptyWatchList from "./EmptyWatchList";
+import React from "react";
+
+interface SubscriptionsProps {}
+
+const Subscriptions: React.FC<SubscriptionsProps> = () => {
+  const dispatchFun = useDispatch();
+  const subscriptionsList = useSelector(
+    (store: any) => store.subscriptions.value
+  );
+  const user = JSON.parse(localStorage.getItem("user")!);
+
+  const remove = async (item: string) => {
+    const index = subscriptionsList.findIndex((channel: string) => channel === item);
+    dispatchFun(removeFromSubscriptionsList(index));
+
+    await fetch(
+      "https://nobore-backend.onrender.com/api/subscriptions/removeFromList",
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({
+          email: user.email,
+          channelName: item,
+        }),
+      }
+    );
+  };
+
+  if (subscriptionsList.length === 0) {
+    return <EmptyWatchList message="No subscriptions yet" />;
+  }
+
+  return (
+    <div className="w-full ">
+      <h1 className="text-2xl w-1/2 mx-auto text-center p-4 font-bold">
+        Subscriptions
+      </h1>
+      <div className="h-fit w-4/5 mx-auto grid grid-cols-1 gap-x-10 gap-y-5 md:grid-cols-2 p-7 my-1">
+        {subscriptionsList.map((channel: string) => (
+          <div
+            key={channel}
+            className="flex-shrink-0  border rounded-lg shadow-sm h-fit py-5 pl-5 pr-10 group block"
+          >
+            <div className="flex wrap items-center justify-between">
+              <div className="flex gap-6 items-center">
+                <div className="avatar">
+                  <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img
+                      alt="user"
+                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    />
+                  </div>
+                </div>
+                <h3 className="font-semibold dark:text-white text-gray-800">
+                  {channel}
+                </h3>
+              </div>
+              <button
+                onClick={() => remove(channel)}
+                className="btn rounded-badge dark:hover:bg-gray-600 dark:text-white btn-outline"
+              >
+                Unsubscribe
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Subscriptions;
---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,66 @@
+import {formatDistanceToNow} from "date-fns/formatDistanceToNow";
+import { formatCount } from "../utils/helper";
+import React from "react";
+
+interface VideoCardProps {
+  items: {
+    snippet: {
+      channelTitle: string;
+      title: string;
+      thumbnails: {
+        medium: {
+          url: string;
+        };
+      };
+      publishedAt: string;
+    };
+    statistics: {
+      viewCount: number;
+    };
+  };
+}
+
+const VideoCard: React.FC<VideoCardProps> = ({ items }) => {
+  const { channelTitle, title, thumbnails, publishedAt } = items.snippet;
+  const { viewCount } = items.statistics;
+  const { medium } = thumbnails;
+  const date = publishedAt.slice(0, 10);
+  const formattedViews = formatCount(viewCount);
+  const formattedDate = formatDistanceToNow(date, {
+    addSuffix: true,
+  }).replace("about ", "");
+
+  return (
+    <div className="w-[350px] flex flex-col justify-between font-mukta cursor-pointer hover:shadow-xl rounded-lg h-80 my-4 mx-3 p-3">
+      <div>
+        <img alt="thumbnail" className="rounded-lg" src={medium.url}></img>
+        <div className="p-1 text-lg font-semibold line-clamp-2">{title}</div>
+      </div>
+      <div className="flex mt-1 pr-1 items-center justify-between">
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              fillRule="evenodd"
+              d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <div>
+            <div className=" ml-1 font-semibold text-sm">{channelTitle}</div>
+            <div className=" ml-1 font-light text-sm">
+              {formattedViews} views
+            </div>
+          </div>
+        </div>
+        <div className=" font-semibold text-sm">{formattedDate}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
